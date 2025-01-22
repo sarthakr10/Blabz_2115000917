b7976e885705d8f14bd896b770c2b10ed78bc9f6
sarthakr10 <rastogisarthak35@gmail.com>
2025-01-23 01:45:13 +0530

    multiplication table

---
+++ l1q18.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class l1q18 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        for (int i = 6; i <= 9; i++) {
+            System.out.println(number + " * " + i + " = " + (number * i));
+        }
+
+        scanner.close();
+    }
+}
