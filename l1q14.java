2cb46501af0e1570099b1b453d0e157ec8e3cb2b
sarthakr10 <rastogisarthak35@gmail.com>
2025-01-23 01:38:56 +0530

    factorial of an integer

---
+++ l1q14.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class l1q14 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int number = scanner.nextInt();
+
+        if (number < 0) {
+            System.out.println("Please enter a positive integer.");
+        } else {
+            int factorial = 1;
+            int i = 1;
+            while (i <= number) {
+                factorial *= i;
+                i++;
+            }
+            System.out.println("The factorial of " + number + " is " + factorial);
+        }
+        scanner.close();
+    }
+}
