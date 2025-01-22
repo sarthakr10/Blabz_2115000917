4d326ceff68705048db73a151a52756530e81600
sarthakr10 <rastogisarthak35@gmail.com>
2025-01-23 01:11:59 +0530

    first number the smallest?

---
+++ l1q2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class l1q2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int number1 = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int number2 = scanner.nextInt();
+
+        System.out.print("Enter the third number: ");
+        int number3 = scanner.nextInt();
+
+        boolean isFirstSmallest = (number1 < number2) && (number1 < number3);
+
+        System.out.println("Is the first number the smallest? " + isFirstSmallest);
+    }
+}
