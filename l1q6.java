173b87a50e88f1790a9169487c80f9b01e5fe137
sarthakr10 <rastogisarthak35@gmail.com>
2025-01-23 01:17:50 +0530

    whether a number is positive, negative, or zero

---
+++ l1q6.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class l1q6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int number = scanner.nextInt();
+
+        if (number > 0) {
+            System.out.println("positive");
+        } else if (number < 0) {
+            System.out.println("negative");
+        } else {
+            System.out.println("zero");
+        }
+
+    }
+}
