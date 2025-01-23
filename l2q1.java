dfff1a4430f415e625747d6224de13a4f4b42803
sarthakr10 <rastogisarthak35@gmail.com>
2025-01-23 11:18:58 +0530

    LeapYear program

---
+++ l2q1.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class l2q1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a year: ");
+        int year = scanner.nextInt();
+
+        if (year < 1582) {
+            System.out.println("The year must be 1582 or later.");
+        } else {
+            if (year % 400 == 0) {
+                System.out.println(year + " is a Leap Year.");
+            } else if (year % 100 == 0) {
+                System.out.println(year + " is not a Leap Year.");
+            } else if (year % 4 == 0) {
+                System.out.println(year + " is a Leap Year.");
+            } else {
+                System.out.println(year + " is not a Leap Year.");
+            }
+
+            if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+                System.out.println(year + " is a Leap Year.");
+            } else {
+                System.out.println(year + " is not a Leap Year.");
+            }
+        }
+
+        scanner.close();
+    }
+}
