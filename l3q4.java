46b4695b8a2f1d4dac774e0be399092114f331b1
sarthakr10 <rastogisarthak35@gmail.com>
2025-01-23 12:10:40 +0530

    Abundant Number

---
+++ l3q4.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class l3q4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int number = scanner.nextInt();
+        int sum = 0;
+
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+
+        if (sum > number) {
+            System.out.println(number + " is an Abundant Number.");
+        } else {
+            System.out.println(number + " is Not an Abundant Number.");
+        }
+
+        scanner.close();
+    }
+}
