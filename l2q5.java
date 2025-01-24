3590f600745e0c64f04a04affd0a07e065a6925a
sarthakr10 <rastogisarthak35@gmail.com>
2025-01-25 00:48:23 +0530

    digits array in reverse order

---
+++ l2q5.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class l2q5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        
+        int temp = number;
+        int count = 0;
+        while (temp != 0) {
+            temp /= 10;
+            count++;
+        }
+        
+        int[] digits = new int[count];
+        temp = number;
+        for (int i = 0; i < count; i++) {
+            digits[i] = temp % 10;
+            temp /= 10;
+        }
+        
+        int[] reversedDigits = new int[count];
+        for (int i = 0; i < count; i++) {
+            reversedDigits[i] = digits[count - 1 - i];
+        }
+        
+        System.out.print("Reversed number: ");
+        for (int i = 0; i < count; i++) {
+            System.out.print(reversedDigits[i]);
+        }
+    }
+}
