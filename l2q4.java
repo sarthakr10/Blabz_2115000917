7f64687ce19f993b6ba470b11bf0eae0a7b51170
sarthakr10 <rastogisarthak35@gmail.com>
2025-01-25 00:46:18 +0530

    maxDigit

---
+++ l2q4.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class l2q4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        long number = scanner.nextLong();
+        scanner.close();
+
+        int maxDigit = 10;
+        int[] digits = new int[maxDigit];
+        int index = 0;
+
+        while (number > 0) {
+            if (index == maxDigit) {
+                maxDigit += 10;
+                int[] temp = new int[maxDigit];
+                System.arraycopy(digits, 0, temp, 0, digits.length);
+                digits = temp;
+            }
+            digits[index++] = (int) (number % 10);
+            number /= 10;
+        }
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int i = 0; i < index; i++) {
+            if (digits[i] > largest) {
+                secondLargest = largest;
+                largest = digits[i];
+            } else if (digits[i] > secondLargest && digits[i] != largest) {
+                secondLargest = digits[i];
+            }
+        }
+
+        System.out.println("Largest digit: " + largest);
+        System.out.println("Second largest digit: " + secondLargest);
+    }
+}
