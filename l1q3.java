a4c76f82ba3d423b300600b7ddf861fdee5903ce
sarthakr10 <rastogisarthak35@gmail.com>
2025-01-27 13:59:16 +0530

    calculate the maximum number of possible handshakes

---
+++ l1q3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class l1q3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter the number of students: ");
+        int numberOfStudents = scanner.nextInt();
+        
+        int numberOfHandshakes = (numberOfStudents * (numberOfStudents - 1)) / 2;
+        
+        System.out.println("The maximum number of possible handshakes is: " + numberOfHandshakes);
+        
+        scanner.close();
+    }
+}
