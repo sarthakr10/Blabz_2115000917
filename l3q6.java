90a74340585e3a515d13679704bc2c8c66407b74
sarthakr10 <rastogisarthak35@gmail.com>
2025-01-28 12:15:03 +0530

    NumberChecker

---
+++ l3q6.java
@@ -0,0 +1,103 @@
+public class l3q6 {
+
+    public static void main(String[] args) {
+        int number = 28;
+        int[] factors = findFactors(number);
+        
+        System.out.println("Factors: ");
+        for (int factor : factors) {
+            System.out.print(factor + " ");
+        }
+        System.out.println();
+
+        System.out.println("Greatest Factor: " + findGreatestFactor(factors));
+        System.out.println("Sum of Factors: " + sumOfFactors(factors));
+        System.out.println("Product of Factors: " + productOfFactors(factors));
+        System.out.println("Product of Cube of Factors: " + productOfCubeOfFactors(factors));
+        System.out.println("Is Perfect Number: " + isPerfectNumber(number));
+        System.out.println("Is Abundant Number: " + isAbundantNumber(number));
+        System.out.println("Is Deficient Number: " + isDeficientNumber(number));
+    }
+
+    public static int[] findFactors(int number) {
+        int count = 0;
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                count++;
+            }
+        }
+
+        int[] factors = new int[count];
+        int index = 0;
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                factors[index++] = i;
+            }
+        }
+        return factors;
+    }
+
+    public static int findGreatestFactor(int[] factors) {
+        int greatest = factors[0];
+        for (int factor : factors) {
+            if (factor > greatest) {
+                greatest = factor;
+            }
+        }
+        return greatest;
+    }
+
+    public static int sumOfFactors(int[] factors) {
+        int sum = 0;
+        for (int factor : factors) {
+            sum += factor;
+        }
+        return sum;
+    }
+
+    public static int productOfFactors(int[] factors) {
+        int product = 1;
+        for (int factor : factors) {
+            product *= factor;
+        }
+        return product;
+    }
+
+    public static double productOfCubeOfFactors(int[] factors) {
+        double product = 1;
+        for (int factor : factors) {
+            product *= Math.pow(factor, 3);
+        }
+        return product;
+    }
+
+    public static boolean isPerfectNumber(int number) {
+        int sum = 0;
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+        return sum == number;
+    }
+
+    public static boolean isAbundantNumber(int number) {
+        int sum = 0;
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+        return sum > number;
+    }
+
+    public static boolean isDeficientNumber(int number) {
+        int sum = 0;
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+        return sum < number;
+    }
+}
