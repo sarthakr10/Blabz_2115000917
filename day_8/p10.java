ca59cae2bb9a724bc378af68f20f6c4c6627d7cc
sarthakr10 <rastogisarthak35@gmail.com>
2025-01-29 14:04:25 +0530

    Remove a Specific Character from a String

---
+++ day_8/p10.java
@@ -0,0 +1,12 @@
+public class p10 {
+    public static void main(String[] args) {
+        String input = "Hello World";
+        char charToRemove = 'l';
+        String result = removeCharacter(input, charToRemove);
+        System.out.println("Modified String: " + result);
+    }
+
+    public static String removeCharacter(String str, char ch) {
+        return str.replace(Character.toString(ch), "");
+    }
+}
