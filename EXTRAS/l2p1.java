b0f4445526f9ed79bbf1830566c6ae92fdec6f93
sarthakr10 <rastogisarthak35@gmail.com>
2025-01-29 11:05:11 +0530

    Time Zones and ZonedDateTime

---
+++ EXTRAS/l2p1.java
@@ -0,0 +1,17 @@
+import java.time.ZonedDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+public class l2p1 {
+    public static void main(String[] args) {
+        ZonedDateTime gmtTime = ZonedDateTime.now(ZoneId.of("GMT"));
+        ZonedDateTime istTime = ZonedDateTime.now(ZoneId.of("Asia/Kolkata"));
+        ZonedDateTime pstTime = ZonedDateTime.now(ZoneId.of("America/Los_Angeles"));
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z");
+
+        System.out.println("Current time in GMT: " + gmtTime.format(formatter));
+        System.out.println("Current time in IST: " + istTime.format(formatter));
+        System.out.println("Current time in PST: " + pstTime.format(formatter));
+    }
+}
