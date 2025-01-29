1ccba72a06e320e30befe2c82bb174851ece0d0f
sarthakr10 <rastogisarthak35@gmail.com>
2025-01-29 14:06:58 +0530

    checks if the two strings are anagrams of each other

---
+++ day_8/p11.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class p11 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first string: ");
+        String str1 = scanner.nextLine();
+
+        System.out.print("Enter the second string: ");
+        String str2 = scanner.nextLine();
+
+        if (areAnagrams(str1, str2)) {
+            System.out.println("The strings are anagrams.");
+        } else {
+            System.out.println("The strings are not anagrams.");
+        }
+
+        scanner.close();
+    }
+
+    public static boolean areAnagrams(String str1, String str2) {
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        char[] charArray1 = str1.toCharArray();
+        char[] charArray2 = str2.toCharArray();
+
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+
+        return Arrays.equals(charArray1, charArray2);
+    }
+}
