1f8234b83d1e8c789d6c0efb72b3efd9e6ba5cc4
sarthakr10 <rastogisarthak35@gmail.com>
2025-01-30 11:54:33 +0530

    Toggle Case of Characters

--- day_8/p7.java
+++ day_8/p7.java
@@ -6,16 +6,15 @@ public class p7 {
     }
 
     public static String toggleCase(String str) {
-        StringBuilder result = new StringBuilder(str.length());
-        for (char c : str.toCharArray()) {
+        char[] chars = str.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
             if (Character.isUpperCase(c)) {
-                result.append(Character.toLowerCase(c));
+            chars[i] = Character.toLowerCase(c);
             } else if (Character.isLowerCase(c)) {
-                result.append(Character.toUpperCase(c));
-            } else {
-                result.append(c);
+            chars[i] = Character.toUpperCase(c);
             }
         }
-        return result.toString();
+        return new String(chars);
     }
 }
