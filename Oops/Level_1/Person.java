b81aacef8eaf916218776fc1f2e866e10281111e
sarthakr10 <rastogisarthak35@gmail.com>
2025-01-31 15:36:41 +0530

    public class Person {

---
+++ Oops/Level_1/Person.java
@@ -0,0 +1,55 @@
+public class Person {
+    private String name;
+    private int age;
+
+    // Default constructor
+    public Person() {
+    }
+
+    // Parameterized constructor
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Copy constructor
+    public Person(Person other) {
+        this.name = other.name;
+        this.age = other.age;
+    }
+
+    // Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{name='" + name + "', age=" + age + "}";
+    }
+    public static void main(String[] args) { 
+        Person person1 = new Person();
+        person1.setName("Alice");
+        person1.setAge(30);
+        System.out.println(person1);
+
+        Person person2 = new Person("Bob", 25);
+        System.out.println(person2);
+
+        // Using copy constructor
+        Person person3 = new Person(person2);
+        System.out.println(person3);
+    }
+}
