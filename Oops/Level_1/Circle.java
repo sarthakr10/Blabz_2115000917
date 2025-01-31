b043891003fe66b8aac3dafc0bd9cb6e3023e5e9
sarthakr10 <rastogisarthak35@gmail.com>
2025-01-31 15:17:05 +0530

    Circle

---
+++ Oops/Level_1/Circle.java
@@ -0,0 +1,27 @@
+public class Circle {
+    private double radius;
+
+    public Circle() {
+        this(1.0);
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    public double getCircumference() {
+        return 2 * Math.PI * radius;
+    }
+}
