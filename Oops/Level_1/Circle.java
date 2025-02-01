140fd47c8231ae0c9fc8acdd7f64122e882433c9
sarthakr10 <rastogisarthak35@gmail.com>
2025-02-01 12:07:55 +0530

    Circle

--- Oops/Level_1/Circle.java
+++ Oops/Level_1/Circle.java
@@ -24,4 +24,18 @@ public class Circle {
     public double getCircumference() {
         return 2 * Math.PI * radius;
     }
+    public static void main(String[] args) {
+        Circle circle1 = new Circle();
+        Circle circle2 = new Circle(2.5);
+
+        System.out.println("Circle 1:");
+        System.out.println("Radius: " + circle1.getRadius());
+        System.out.println("Area: " + circle1.getArea());
+        System.out.println("Circumference: " + circle1.getCircumference());
+
+        System.out.println("\nCircle 2:");
+        System.out.println("Radius: " + circle2.getRadius());
+        System.out.println("Area: " + circle2.getArea());
+        System.out.println("Circumference: " + circle2.getCircumference());
+    }
 }
