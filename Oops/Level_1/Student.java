81ec6470c1c385c360386461e7961f74eec8571a
sarthakr10 <rastogisarthak35@gmail.com>
2025-02-02 16:03:34 +0530

    University Management System

---
+++ Oops/Level_1/Student.java
@@ -0,0 +1,43 @@
+public class Student {
+    public int rollNumber;
+    protected String name;
+    private double CGPA;
+
+    public Student(int rollNumber, String name, double CGPA) {
+        this.rollNumber = rollNumber;
+        this.name = name;
+        this.CGPA = CGPA;
+    }
+
+    public double getCGPA() {
+        return CGPA;
+    }
+
+    public void setCGPA(double CGPA) {
+        this.CGPA = CGPA;
+    }
+}
+
+class PostgraduateStudent extends Student {
+    private String thesisTitle;
+
+    public PostgraduateStudent(int rollNumber, String name, double CGPA, String thesisTitle) {
+        super(rollNumber, name, CGPA);
+        this.thesisTitle = thesisTitle;
+    }
+
+    public String getThesisTitle() {
+        return thesisTitle;
+    }
+
+    public void setThesisTitle(String thesisTitle) {
+        this.thesisTitle = thesisTitle;
+    }
+
+    public void displayDetails() {
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Name: " + name);
+        System.out.println("CGPA: " + getCGPA());
+        System.out.println("Thesis Title: " + thesisTitle);
+    }
+}
