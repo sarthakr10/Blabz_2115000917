9c587cff3a27e3aeb19b95b25469ee4b234d5531
sarthakr10 <rastogisarthak35@gmail.com>
2025-02-02 16:09:46 +0530

    Employee Records

---
+++ Oops/Level_1/Employee.java
@@ -0,0 +1,30 @@
+public class Employee {
+    public int employeeID;
+    protected String department;
+    private double salary;
+
+    public Employee(int employeeID, String department, double salary) {
+        this.employeeID = employeeID;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
+
+class Manager extends Employee {
+    public Manager(int employeeID, String department, double salary) {
+        super(employeeID, department, salary);
+    }
+
+    public void displayDetails() {
+        System.out.println("Employee ID: " + employeeID);
+        System.out.println("Department: " + department);
+    }
+}
