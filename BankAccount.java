b7f5d239980dd5e819d1b46aa801edd0886652e3
sarthakr10 <rastogisarthak35@gmail.com>
2025-02-04 12:06:47 +0530

    Bank Account System

--- BankAccount.java
+++ BankAccount.java
@@ -1,47 +1,33 @@
-public class BankAccount {
-    private static String bankName = "Global Bank";
+public class BankAccount{
+    private static String bankName = "Global bank";
     private static int totalAccounts = 0;
-
+    
     private String accountHolderName;
-    private final String accountNumber;
+    private final String  accountNumber;
 
-    public BankAccount(String accountHolderName, String accountNumber) {
+    public BankAccount(String accountHolderName , String accountNumber){
         this.accountHolderName = accountHolderName;
         this.accountNumber = accountNumber;
         totalAccounts++;
     }
-
-    public static int getTotalAccounts() {
+    public static int getTotalAccounts(){
         return totalAccounts;
     }
-
-    public String getAccountHolderName() {
-        return accountHolderName;
-    }
-
-    public String getAccountNumber() {
-        return accountNumber;
-    }
-
-    public static String getBankName() {
-        return bankName;
-    }
-
-    public void displayAccountDetails() {
+    public void display(){
         if (this instanceof BankAccount) {
-            System.out.println("Bank Name: " + bankName);
-            System.out.println("Account Holder Name: " + accountHolderName);
-            System.out.println("Account Number: " + accountNumber);
+            System.out.println("Bank name : "+ bankName);
+            System.out.println("Account Holder name : "+accountHolderName);
+            System.out.println("Account number : "+accountNumber);
+        }
+        else{
+            System.out.println("Object is not an instance of BankAccount class");
         }
     }
-
     public static void main(String[] args) {
-        BankAccount account1 = new BankAccount("John Doe", "123456789");
-        BankAccount account2 = new BankAccount("Jane Smith", "987654321");
+        BankAccount b1 = new BankAccount("Ayan", "123456789");
+        BankAccount b2  = new BankAccount("SRK", "123412221");
 
-        account1.displayAccountDetails();
-        account2.displayAccountDetails();
-
-        System.out.println("Total Accounts: " + BankAccount.getTotalAccounts());
+        b1.display();;
+        b2.display();
     }
 }
